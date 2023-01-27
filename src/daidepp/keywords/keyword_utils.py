18360da7014b987f8167cf2e9d730681cf0a5ef2
Feng Gu <fenggu2000@gmail.com>
2023-01-27 15:50:30 -0500

    bug fix

--- src/daidepp/keywords/keyword_utils.py
+++ src/daidepp/keywords/keyword_utils.py
@@ -2,7 +2,7 @@ def and_items(items):
     if len(items) == 1:
         return str(items[0]) + " "
     elif len(items) == 2:
-        return str(items[0]) + "and " + str(items[1]) + " "
+        return str(items[0]) + " and " + str(items[1]) + " "
     else:
         return ", ".join([str(item) for item in items[:-1]]) + ", and " + str(items[-1]) + " "
 
@@ -10,6 +10,6 @@ def or_items(items):
     if len(items) == 1:
         return str(items[0]) + " "
     elif len(items) == 2:
-        return str(items[0]) + "or " + str(items[1]) + " "
+        return str(items[0]) + " or " + str(items[1]) + " "
     else:
         return ", ".join([str(item) for item in items[:-1]]) + ", or " + str(items[-1]) + " "
